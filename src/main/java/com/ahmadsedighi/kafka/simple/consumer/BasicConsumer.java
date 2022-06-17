37f0d61cc196705a74c5fa09017d46710bfb0f59
ahmadseddighi@gmail.com <ahmadseddighi@gmail.com>
2022-06-17 16:19:41 +0100

    Downgrade kafka-client version

--- src/main/java/com/ahmadsedighi/kafka/simple/consumer/BasicConsumer.java
+++ src/main/java/com/ahmadsedighi/kafka/simple/consumer/BasicConsumer.java
@@ -3,7 +3,9 @@ package com.ahmadsedighi.kafka.simple.consumer;
 import com.ahmadsedighi.kafka.simple.Constants;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
 import java.util.Map;
@@ -18,8 +20,8 @@ import java.util.Set;
 public class BasicConsumer {
     public static void main(String[] args) {
         final var topic = Constants.TOPIC_NAME;
-        final Map<String, Object> config = Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092", ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
+        final Map<String, Object> config = Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
                 ConsumerConfig.GROUP_ID_CONFIG, "basic-consumer-sample",
 //                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
