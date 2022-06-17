dcb110ab9f298eb5a90d28ff4aa533c4a82aff3b
ahmadseddighi@gmail.com <ahmadseddighi@gmail.com>
2022-06-17 13:59:26 +0100

    Remove auto-offset reset

--- src/main/java/com/ahmadsedighi/kafka/simple/consumer/BasicConsumer.java
+++ src/main/java/com/ahmadsedighi/kafka/simple/consumer/BasicConsumer.java
@@ -22,7 +22,7 @@ public class BasicConsumer {
                 "localhost:9092", ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
                 ConsumerConfig.GROUP_ID_CONFIG, "basic-consumer-sample",
-                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+//                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
                 ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         try (var consumer = new KafkaConsumer<String, String>(config)) {
             consumer.subscribe(Set.of(topic));
